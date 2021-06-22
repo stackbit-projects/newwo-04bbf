f2608aedeeb81622ec33cec6489e83dd6fd44967
Stackbit Code Editor <projects@stackbit.com>
2021-06-22 01:31:35 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -20,7 +20,7 @@ export default class SectionHero extends React.Component {
                     <div className="grid">
                         {image && (
                             <div className="cell block-preview">
-                                <img src={withPrefix(image)} alt={imageAlt} />
+                                <img src={withPrefix(image)} alt={imageAlt} style="color:#4a54f1"/>
                             </div>
                         )}
                         <div className="cell block-content">
